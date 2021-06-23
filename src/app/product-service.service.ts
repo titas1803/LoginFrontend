d90251be9736598513953ba6fad3414468799bfd
SoumenduMaitra <soumendu.maitra1998@gmail.com>
2021-06-23 10:31:25 +0530

    commit

--- src/app/product-service.service.ts
+++ src/app/product-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProdDto } from './prod-dto';
@@ -12,7 +12,8 @@ export class ProductServiceService {
 
   viewbyid(pid:number):Observable<any>{
     console.log("In Viewbyid");
-    return this.http.get("http://localhost:8082/loginmodule/viewprodbyid/"+pid);
+    let token: string = JSON.parse(localStorage.getItem("userinfo")!).token;
+    return this.http.get("http://localhost:8082/loginmodule/viewprodbyid/"+pid,{ headers: new HttpHeaders({ 'token-id': token }) });
   }
 
   addproduct(prod:ProdDto):Observable<any>{
